6f20e8f5c4d003fdb44f27e999a3e29b7fe54b37
Anurag <anuragupadhayay002@gmail.com>
2026-03-21 11:28:59 +0530

    This is my First commit

---
+++ src/DefaultPackage/Test1.java
@@ -0,0 +1,13 @@
+package DefaultPackage;
+
+public class Test1 {
+public static void main(String [] args) {
+	String Hungry="Yes";
+	
+	if(Hungry == "NO")
+		System.out.println("Eat something");
+	else {
+		System.out.println("Do work");
+	}
+}
+}
