c08f5902223cfefa86897a2fa92c652b8daba7f4
Anurag <anuragupadhayay002@gmail.com>
2026-03-23 09:36:44 +0530

    Added Additional Checks.

--- src/DefaultPackage/Test2.java
+++ src/DefaultPackage/Test2.java
@@ -4,7 +4,7 @@ public class Test2 {
 public static void main(String [] args) {
 	String Hungry="Yes";
 	
-	if(Hungry == "NO")
+	if(Hungry == "NO"  || Hungry=="Y" || Hungry=="y")
 		System.out.println("Eat something");
 	else {
 		System.out.println("Do work");
